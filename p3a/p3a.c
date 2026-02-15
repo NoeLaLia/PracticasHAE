b0b4830c6d0bb562dbb74732af47cbe16de765cb
NoeLaLia <162318566+NoeLaLia@users.noreply.github.com>
2026-02-15 16:15:41 +0100

    Initialize ADCON1 in main function
    Added ADCON1 initialization for analog input configuration.

--- p3a/p3a.c
+++ p3a/p3a.c
@@ -1,18 +1,19 @@
 /*
-Práctica 3 a): El diodo emisor de luz (led) conectado a la patilla RB3 debe cambiar de estado
+PrÃ¡ctica 3 a): El diodo emisor de luz (led) conectado a la patilla RB3 debe cambiar de estado
 (encendido/apagado) cada vez que se presiona el pulsador SW conectado al terminal RB2.
-Hay que resolver este apartado haciendo que el microcontrolador observe periódicamente
-(cada 100mseg.) si el pulsador está presionado o no (técnica de polling)
+Hay que resolver este apartado haciendo que el microcontrolador observe periÃ³dicamente
+(cada 100mseg.) si el pulsador estÃ¡ presionado o no (tÃ©cnica de polling)
 Componentes ISIS: PIC18F452, RES, LED-BLUE y BUTTON
-Preguntas: ¿Cuántas veces por segundo crees que el µC
+Preguntas: Â¿CuÃ¡ntas veces por segundo crees que el ÂµC
 debe observar el valor de RB2 para que sepa todas las
-veces que se pulsa el botón?... ¿seguro?
-¿Cuál es el tiempo mínimo que puedes mantener pulsado
-un botón?... ¿de verdad?... ¿seguro?
+veces que se pulsa el botÃ³n?... Â¿seguro?
+Â¿CuÃ¡l es el tiempo mÃ­nimo que puedes mantener pulsado
+un botÃ³n?... Â¿de verdad?... Â¿seguro?
 */
 
 void main() {
             char botonPulsado = 0;
+            ADCON1 = 0x07;
             TRISB.B2 = 1;
             TRISB.B3 = 0;
             while(1){
@@ -27,4 +28,5 @@ void main() {
                         delay_ms(25);
 
   }
+
 }
